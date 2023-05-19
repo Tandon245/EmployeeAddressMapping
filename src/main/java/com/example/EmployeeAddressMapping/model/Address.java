3ccb208ac97242aaf533f37d512022d598238f5b
Tandon245 <tandonabhishek245@gmail.com>
2023-05-20 00:48:13 +0530

    Initial commit

---
+++ src/main/java/com/example/EmployeeAddressMapping/model/Address.java
@@ -0,0 +1,29 @@
+package com.example.EmployeeAddressMapping.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipcode;
+}
---
+++ src/main/java/com/example/EmployeeAddressMapping/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.EmployeeAddressMapping.repository;
+
+import com.example.EmployeeAddressMapping.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
